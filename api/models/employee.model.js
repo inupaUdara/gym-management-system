a0cee42a75e8daaa3692773f677415243a1e180d
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-02-26 23:40:58 +0530

    create employee model

---
+++ api/models/employee.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+
+const Employee = mongoose.model("Employee", employeeSchema);
+export default Employee;
