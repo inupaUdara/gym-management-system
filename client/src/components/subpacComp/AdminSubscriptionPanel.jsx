8c4774ce1d857694496aa233c1fc3f304f7c6681
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-04-10 14:32:17 +0530

    modified:   client/src/components/subpacComp/AdminSubscriptionPanel.jsx

--- client/src/components/subpacComp/AdminSubscriptionPanel.jsx
+++ client/src/components/subpacComp/AdminSubscriptionPanel.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { Spinner, Modal, Button } from "flowbite-react";
+import { useSelector } from "react-redux";
+import { Modal, Button } from "flowbite-react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { BsInfoCircle } from "react-icons/bs";
@@ -11,13 +12,15 @@ import { useSnackbar } from 'notistack';
 
 const AdminSubscriptionPanel = () => {
   const [subPackages, setSubPackage] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
   const [loading, setLoading] = useState(false);
   const [subPackageDelete, setSubPackageDelete] = useState("");
   const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     setLoading(true);
-    axios
+    if (currentUser.isAdmin || currentUser.role === "Manager") {
+      axios
       .get(`/api/subpackage/getSubPackage`)
       .then((response) => {
         setSubPackage(response.data.data)
@@ -26,8 +29,9 @@ const AdminSubscriptionPanel = () => {
       .catch((error) => {
         console.log(error);
         setLoading(false);
-      })
-  }, []);
+      }); 
+    }
+  }, [currentUser._id]);
 
   const handledeletePackage = () => {
     setLoading(true);
@@ -48,74 +52,76 @@ const AdminSubscriptionPanel = () => {
 
   return (
     <div className="p-4">
-      <div className="p-2 m-2">
-        <Link to={"/subpackages/create"}>
-            <button className="focus:outline-none font-bold text-white bg-red-700 hover:bg-red-800 focus:ring-4 rounded-lg text-sm px-5 py-3 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"> 
-              Create New Package
-            </button>
-        </Link>
-      </div>
-      {loading ? (
-          <Spinner size="md" color="red-800" />
-      ):(
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr>
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">Package Name</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">Price</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">Valid Time</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">Description</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">Specific</th>
-              <th className="border border-slate-600 rounded-md">Operations</th>
-            </tr>
-          </thead>
-          <tbody>
-            {subPackages.map((subPackage, index) => (
-              <tr key={subPackage._id} className="h-8">
-                <td className="border border-slate-700 rounded-md text-center">
-                  {index + 1}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  {subPackage.subPackageName}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {subPackage.price}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {subPackage.validTime}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {subPackage.description}
-                </td>
-                <td className="border border-slate-700 rounded-md max-md:hidden">
-                  <ul className="max-w-md space-y-1 list-disc list-inside">
-                    <li>{subPackage.note1}</li>
-                    <li>{subPackage.note2}</li>
-                    <li>{subPackage.note3}</li>
-                  </ul>
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  <div className="flex justify-center gap-x-4">
-                    <Link to={`/subpackages/details/${subPackage._id}`}>
-                      <BsInfoCircle className="text-2xl text-green-800"/>
-                    </Link>
-                    <Link to={`/subpackages/edit/${subPackage._id}`}>
-                      <AiOutlineEdit className="text-2xl text-yellow-600"/>
-                    </Link>
-                    <Link >
-                      <MdOutlineDelete onClick={() => {
-                        setLoading(true);
-                        setSubPackageDelete(subPackage._id);
-                      }}
-                       className="text-2xl text-red-600"/>
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      {currentUser.isAdmin || currentUser.role === "Manager" ? (
+        <>
+          <div className="p-2 m-2">
+            <Link to={"/subpackages/create"}>
+                <button className="focus:outline-none font-bold text-white bg-red-700 hover:bg-red-800 focus:ring-4 rounded-lg text-sm px-5 py-3 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"> 
+                  Create New Package
+                </button>
+            </Link>
+          </div>
+            <table className="w-full border-separate border-spacing-2">
+              <thead>
+                <tr>
+                  <th className="border border-slate-600 rounded-md">No</th>
+                  <th className="border border-slate-600 rounded-md">Package Name</th>
+                  <th className="border border-slate-600 rounded-md max-md:hidden">Price</th>
+                  <th className="border border-slate-600 rounded-md max-md:hidden">Valid Time</th>
+                  <th className="border border-slate-600 rounded-md max-md:hidden">Description</th>
+                  <th className="border border-slate-600 rounded-md max-md:hidden">Specific</th>
+                  <th className="border border-slate-600 rounded-md">Operations</th>
+                </tr>
+              </thead>
+              <tbody>
+                {subPackages.map((subPackage, index) => (
+                  <tr key={subPackage._id} className="h-8">
+                    <td className="border border-slate-700 rounded-md text-center">
+                      {index + 1}
+                    </td>
+                    <td className="border border-slate-700 rounded-md text-center">
+                      {subPackage.subPackageName}
+                    </td>
+                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                      {subPackage.price}
+                    </td>
+                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                      {subPackage.validTime}
+                    </td>
+                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">
+                      {subPackage.description}
+                    </td>
+                    <td className="border border-slate-700 rounded-md max-md:hidden">
+                      <ul className="max-w-md space-y-1 list-disc list-inside">
+                        <li>{subPackage.note1}</li>
+                        <li>{subPackage.note2}</li>
+                        <li>{subPackage.note3}</li>
+                      </ul>
+                    </td>
+                    <td className="border border-slate-700 rounded-md text-center">
+                      <div className="flex justify-center gap-x-4">
+                        <Link to={`/subpackages/details/${subPackage._id}`}>
+                          <BsInfoCircle className="text-2xl text-green-800"/>
+                        </Link>
+                        <Link to={`/subpackages/edit/${subPackage._id}`}>
+                          <AiOutlineEdit className="text-2xl text-yellow-600"/>
+                        </Link>
+                        <Link >
+                          <MdOutlineDelete onClick={() => {
+                            setLoading(true);
+                            setSubPackageDelete(subPackage._id);
+                          }}
+                          className="text-2xl text-red-600"/>
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+        </>
+      ) : (
+        <p>No packages yet!</p>
       )}
       <Modal
         show={loading}
