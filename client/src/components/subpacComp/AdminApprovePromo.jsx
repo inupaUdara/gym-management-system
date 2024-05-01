d8bb9f7c1690ff447d0b5cf3ce661560c0e1cd73
Yasindu Kalhara Balasooriya <it22561848@my.sliit.lk>
2024-05-01 21:13:30 +0530

    packages design

--- client/src/components/subpacComp/AdminApprovePromo.jsx
+++ client/src/components/subpacComp/AdminApprovePromo.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Modal, Button } from "flowbite-react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { FcApproval } from "react-icons/fc";
-import { MdOutlineDelete } from "react-icons/md";
-import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Table } from "flowbite-react";
 import { useSnackbar } from "notistack";
 
 const AdminApprovePromo = () => {
@@ -50,84 +46,88 @@ const AdminApprovePromo = () => {
   };
 
   return (
-    <div className="p-4">
-      {currentUser.isAdmin || currentUser.role === "Manager" ? (
+    <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+      { currentUser.isAdmin || currentUser.role === "Manager" ? ( 
         <>
-          <table className="w-full border-separate border-spacing-2">
-            <thead>
-              <tr>
-                <th className="border border-slate-600 rounded-md">No</th>
-                <th className="border border-slate-600 rounded-md">Package Name</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Price</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Valid Time</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Description</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Specific</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Start Date</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">End Date</th>
-                <th className="border border-slate-600 rounded-md">Operations</th>
-              </tr>
-            </thead>
-            <tbody>
-              {subPackages.map((subPackage, index) => (
-                <>
-                  {subPackage.Pactype === "PromotionPackage" && (
-                <tr key={subPackage._id} className="h-8">
-                    <td className="border border-slate-700 rounded-md text-center">{index + 1}</td>
-                    <td className="border border-slate-700 rounded-md text-center">{subPackage.subPackageName}</td>
-                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">LKR {subPackage.price}</td>
-                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">{subPackage.validTime}</td>
-                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">{subPackage.description}</td>
-                    <td className="border border-slate-700 rounded-md max-md:hidden">
-                      <ul className="max-w-md space-y-1 list-disc list-inside">
-                        <li>{subPackage.note1}</li>
-                        <li>{subPackage.note2}</li>
-                        <li>{subPackage.note3}</li>
-                      </ul>
-                    </td>
-                    <td className="border border-slate-700 rounded-md max-md:hidden">
-                      {new Date(subPackage.startDate).toLocaleDateString("en-CA")}
-                    </td>
-                    <td className="border border-slate-700 rounded-md max-md:hidden">
-                      {new Date(subPackage.endDate).toLocaleDateString("en-CA")}
-                    </td>
-                    <td className="border border-slate-700 rounded-md text-center">
-                      <div className="flex justify-center gap-x-4">
-                        <FcApproval className="text-2xl text-green-600" />
-                        <MdOutlineDelete
-                          onClick={() => setSubPackageDelete(subPackage._id)}
-                          className="text-2xl text-red-600 cursor-pointer"
-                        />
-                      </div>
-                    </td>
-                  </tr>
-                )}
-                </>
-              ))}
-            </tbody>
-          </table>
+          <Table hoverable className='shadow-md'>
+          <Table.Head>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              No
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Package Name
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Price
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Valid Time
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Description
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Specific
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Start Date
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              End Date
+            </Table.HeadCell>
+            <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+              Status
+            </Table.HeadCell>
+          </Table.Head>
+          {subPackages.map((subPackage, index) => (
+            <>
+              <Table.Body className='divide-y' key={subPackage._id}>
+              {subPackage.Pactype === "PromotionPackage" && (
+                <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800 text-[#1f1f1f]'>
+                  <Table.Cell>
+                    {index + 1}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {subPackage.subPackageName}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {subPackage.price}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {subPackage.validTime}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {subPackage.description}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <ul className="max-w-md space-y-1 list-disc list-inside">
+                      <li>{subPackage.note1}</li>
+                      <li>{subPackage.note2}</li>
+                      <li>{subPackage.note3}</li>
+                    </ul>
+                  </Table.Cell>
+                  <Table.Cell>
+                    {new Date(subPackage.startDate).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {new Date(subPackage.endDate).toLocaleDateString()}
+                  </Table.Cell>
+                  <Table.Cell>
+                    <select>
+                      <option value="Approved">Approved</option>
+                      <option value="Rejected">Rejected</option>
+                    </select>
+                  </Table.Cell>
+                </Table.Row> 
+              )}
+            </Table.Body>
+            </>
+          ))}
+          </Table>
         </>
       ) : (
-        <p>No packages yet!</p>
+        <p> No packages yet!</p>
       )}
-      <Modal show={!!subPackageDelete} onClose={() => setSubPackageDelete("")} popup size="md">
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto" />
-            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this package?
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={() => handleDeletePackage(subPackageDelete)}>
-                Yes, I'm sure
-              </Button>
-              <Button color="gray" onClick={() => setSubPackageDelete("")}>
-                No, cancel
-              </Button>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
     </div>
   );
 };
--- client/src/components/subpacComp/AdminSubscriptionPanel.jsx
+++ client/src/components/subpacComp/AdminSubscriptionPanel.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { Modal, Button } from "flowbite-react";
+import { Modal, Button, Table } from "flowbite-react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { BsInfoCircle } from "react-icons/bs";
@@ -64,58 +64,81 @@ const AdminSubscriptionPanel = () => {
               </button>
             </Link>
           </div>
-          <table className="w-full border-separate border-spacing-2">
-            <thead>
-              <tr>
-                <th className="border border-slate-600 rounded-md">No</th>
-                <th className="border border-slate-600 rounded-md">Package Name</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Price</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Valid Time</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Description</th>
-                <th className="border border-slate-600 rounded-md max-md:hidden">Specific</th>
-                <th className="border border-slate-600 rounded-md">Operations</th>
-              </tr>
-            </thead>
-            <tbody>
+          <div className= 'table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
+            <Table hoverable className='shadow-md'>
+              <Table.Head>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  No
+                </Table.HeadCell>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  Package Name
+                </Table.HeadCell>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  Price
+                </Table.HeadCell>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  Valid Time
+                </Table.HeadCell>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  Description
+                </Table.HeadCell>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  Specific
+                </Table.HeadCell>
+                <Table.HeadCell className="bg-[#1f1f1f] text-[#d4d4d4]">
+                  Operations
+                </Table.HeadCell>
+              </Table.Head>
               {subPackages.map((subPackage, index) => (
                 <>
-                {subPackage.Pactype === "SubscriptionPackage" && (
-                <tr key={subPackage._id} className="h-8">
-              
-                    <td className="border border-slate-700 rounded-md text-center">{index + 1}</td>
-                    <td className="border border-slate-700 rounded-md text-center">{subPackage.subPackageName}</td>
-                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">LKR {subPackage.price}</td>
-                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">{subPackage.validTime}</td>
-                    <td className="border border-slate-700 rounded-md text-center max-md:hidden">{subPackage.description}</td>
-                    <td className="border border-slate-700 rounded-md max-md:hidden">
-                      <ul className="max-w-md space-y-1 list-disc list-inside">
-                        <li>{subPackage.note1}</li>
-                        <li>{subPackage.note2}</li>
-                        <li>{subPackage.note3}</li>
-                      </ul>
-                    </td>
-                    <td className="border border-slate-700 rounded-md text-center">
-                      <div className="flex justify-center gap-x-4">
-                        <Link to={`/subpackages/details/${subPackage._id}`}>
-                          <BsInfoCircle className="text-2xl text-green-800"/>
-                        </Link>
-                        <Link to={`/subpackages/edit/${subPackage._id}`}>
-                          <AiOutlineEdit className="text-2xl text-yellow-600"/>
-                        </Link>
-                        <MdOutlineDelete
-                          onClick={() => {
-                            setSubPackageDelete(subPackage._id);
-                          }}
-                          className="text-2xl text-red-600 cursor-pointer"
-                        />
-                      </div>
-                    </td>
-                </tr>
-                )}
-                </>         
+                  <Table.Body className='divide-y' key={subPackage._id}>
+                  {subPackage.Pactype === "SubscriptionPackage" && (
+                    <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800 text-[#1f1f1f]'>
+                      <Table.Cell>
+                        {index + 1}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {subPackage.subPackageName}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {subPackage.price}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {subPackage.validTime}
+                      </Table.Cell>
+                      <Table.Cell>
+                        {subPackage.description}
+                      </Table.Cell>
+                      <Table.Cell>
+                        <ul className="max-w-md space-y-1 list-disc list-inside">
+                          <li>{subPackage.note1}</li>
+                          <li>{subPackage.note2}</li>
+                          <li>{subPackage.note3}</li>
+                        </ul>
+                      </Table.Cell>
+                      <Table.Cell>
+                        <div className="flex justify-center gap-x-4">
+                          <Link to={`/subpackages/details/${subPackage._id}`}>
+                            <BsInfoCircle className="text-2xl text-green-800"/>
+                          </Link>
+                          <Link to={`/subpackages/edit/${subPackage._id}`}>
+                            <AiOutlineEdit className="text-2xl text-yellow-600"/>
+                          </Link>
+                          <MdOutlineDelete
+                            onClick={() => {
+                              setSubPackageDelete(subPackage._id);
+                            }}
+                            className="text-2xl text-red-600 cursor-pointer"
+                          />
+                        </div>
+                      </Table.Cell>
+                    </Table.Row>
+                  )}
+                  </Table.Body>
+                </>
               ))}
-            </tbody>
-          </table>
+            </Table>
+          </div>
         </>
       ) : (
         <p>No packages yet!</p>
